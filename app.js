51200f14dc83dd935b84058e31a0f5b41e813633
Victor Tirona <vlbtirona@gmail.com>
2024-09-18 18:10:42 +0800

    This is me pushing to GitHub

---
+++ app.js
@@ -0,0 +1,13 @@
+const amount = 12
+
+if (amount < 10){
+    console.log('small number bro');
+}
+
+else{
+    console.log('bro that is huge. soooo huge');
+}
+
+console.log('this is my first node app brodiebabes!!!');
+
+console.log('hello world pareh')
